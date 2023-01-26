824d89dfee6beae3bd5d418ec861db3dc95695e8
bani92 <banseok9204@naver.com>
2023-01-26 12:06:48 +0900

    0126 First commit

---
+++ src/test/java/com/jspo/JspoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
