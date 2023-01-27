e519c1b8f3897526de20f4dff2f9e093ea9267e1
bani92 <banseok9204@naver.com>
2023-01-28 00:16:28 +0900

    0128 [modify] 로그인 구현중

--- src/main/java/com/jspo/member/controller/LoginController.java
+++ src/main/java/com/jspo/member/controller/LoginController.java
@@ -25,28 +25,23 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(MemberDto loginMember, Model m) throws Exception {
-        System.out.println(memberDto);
+
+        // 로그인 페이지에 적은 이메일과 패스워드를 memberDto에 set한다.
          memberDto.setEmail(loginMember.getEmail());
          memberDto.setPwd(loginMember.getPwd());
-        System.out.println(memberDto);
 
+         // memberDto 엔 email값과 pwd 값이 있으며
+        // 그 값을 sql문의 login의 조건으로 select 한다.
         MemberDto result = memberDao.login(memberDto);
-        System.out.println("result "+result); // ServiceImpl
+        if(result != null) {
+            // 결과값이 널이 아니면 즉 select가 정상적으로 되었음
+            // 메인 홈으로 가기
+            return "test";
+        } else {
+            // 로그인 실패시 로그인 페이지에 머무르며
+            // 에러메시지를 띄운다
+            return "redirect:/";
+        }
 
-        System.out.println(loginMember.getEmail());
-        System.out.println(loginMember.getPwd());
-
-        m.addAttribute(memberDto);
-
-
-        // 로그인 (Post)를 눌렀을때
-        // DB에 아이디와 비밀번호를 비교한 값이 있을때 test페이지로 가겠끔
-        // 모델에 담아가지고 DB랑 비교?
-
-        // 로그인 성공시
-
-
-        // 로그인 실패시
-        return "test";
     }
 }
