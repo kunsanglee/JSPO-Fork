25ce9c8fd24c8d5e3df4b3f322d827c3485ad77d
bani92 <banseok9204@naver.com>
2023-02-03 18:01:07 +0900

    0203 [add & modify] 호텔 등록구현

--- src/main/java/com/jspo/hotel/controller/HotelController.java
+++ src/main/java/com/jspo/hotel/controller/HotelController.java
@@ -2,12 +2,16 @@ package com.jspo.hotel.controller;
 
 import com.jspo.hotel.dao.HotelDao;
 import com.jspo.hotel.dto.HotelDto;
+import com.jspo.upload.UploadFileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
 
 @Controller
@@ -15,6 +19,9 @@ public class HotelController {
 
     @Autowired
     private HotelDao hotelDao;
+
+    @Value("${file.dir}")
+    private String uploadPath;
 
     private HotelDto hotelDto = HotelDto.getInstance();
 
@@ -26,10 +33,26 @@ public class HotelController {
 
 
     @PostMapping("/hotel/reg")
-    public String insert(HotelDto hotelDto) throws Exception {
-        System.out.println(hotelDto);
+    public String insert(HotelDto hotelDto,  MultipartFile file) throws Exception {
+
+        String imgUploadPath = uploadPath + "imgUpload";
+        System.out.println("1. imgUploadPath"+imgUploadPath);
+
+        String ymdPath = UploadFileUtils.calcPath(imgUploadPath);
+
+        String fileName = null;
+        System.out.println(file);
+        if (file.getOriginalFilename() != null && (!file.getOriginalFilename().equals(""))) {
+            fileName = UploadFileUtils.fileUpload(imgUploadPath, file.getOriginalFilename(), file.getBytes(), ymdPath);
+            System.out.println("fileName = "+fileName);
+        } else {
+            fileName = uploadPath + File.separator+ "images" + File.separator+ "none.png";
+        }
+
+        hotelDto.setHtImg(File.separator+"imgs"+File.separator+ "imgUpload" + ymdPath + File.separator+ fileName);
         hotelDao.insertHotel(hotelDto);
-        return "HotelList";
+
+        return "redirect:/hotel/list";
     }
 
     @GetMapping("/hotel/list")
@@ -39,4 +62,28 @@ public class HotelController {
        model.addAttribute("list",list);
         return "HotelList";
     }
+
+//    @PostMapping("/upload")
+//    public String Register(HotelDto hotelDto, MultipartFile file) throws Exception {
+//
+//
+//        String imgUploadPath = uploadPath + "imgUpload";
+//        System.out.println("1. imgUploadPath"+imgUploadPath);
+//
+//        String ymdPath = UploadFileUtils.calcPath(imgUploadPath);
+//
+//        String fileName = null;
+//
+//        if (file.getOriginalFilename() != null && (!file.getOriginalFilename().equals(""))) {
+//            fileName = UploadFileUtils.fileUpload(imgUploadPath, file.getOriginalFilename(), file.getBytes(), ymdPath);
+//            System.out.println("fileName = "+fileName);
+//        } else {
+//            fileName = uploadPath + File.separator+ "images" + File.separator+ "none.png";
+//        }
+//
+//        hotelDto.setHtImg(File.separator+"image"+File.separator+ "imgUpload" + ymdPath + File.separator+ fileName);
+//        hotelDao.insertHotel(hotelDto);
+//
+//        return "redirect:/list";
+//    }
 }
